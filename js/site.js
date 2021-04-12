2764f879372ebc5fb22c8866e4fe6b61b2ca9468
Amadeus Martinez <Amadeusrm97@gmail.com>
2021-04-12 16:51:36 -0400

    print numbers from n1 - n2

--- js/site.js
+++ js/site.js
@@ -1,4 +1,13 @@
-//This will do math with numbers
-function doMath(){
-alert("doMath alert");
+//This will print numbers for startNum to endNum
+function printNumbers() {
+    let startNum = parseInt(document.getElementById("numOne").value);
+    let endNum = parseInt(document.getElementById("numTwo").value);
+    let result = "";
+
+    //loop over values from start to end
+    for (let i = startNum; i <= endNum; i++) {
+        result += `${i} `;
+    }
+
+    document.getElementById("result").innerHTML = result;
 }
