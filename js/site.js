2af0cda81783cf838c808f6dee4facd9b01ca8a3
Amadeus Martinez <Amadeusrm97@gmail.com>
2021-04-12 17:47:27 -0400

    done, looks super clean

--- js/site.js
+++ js/site.js
@@ -15,7 +15,7 @@ function displayData(start,end){
      for (let i = start; i <= end; i += 5) {
          const dataRow = document.importNode(myTemplate.content, true);
          dataRow.getElementById("col1").textContent = i;
-         dataRow.getElementById("col2").textContent = i+1;
+         dataRow.getElementById("col2").textContent = i + 1;
          dataRow.getElementById("col3").textContent = i + 2;
          dataRow.getElementById("col4").textContent = i + 3;
          dataRow.getElementById("col5").textContent = i + 4;
